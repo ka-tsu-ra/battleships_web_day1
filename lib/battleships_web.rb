982b75246763ab1828e5fe60e9907f38ebebcd1f
Elin <elinnet@gmail.com>
2015-06-24 16:41:44 +0100

    added placeship.erb file

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -16,13 +16,31 @@ class BattleshipsWeb < Sinatra::Base
 
   get '/getboard' do
     $game = Game.new Player, Board
+    @board = $game.own_board_view $game.player_1
     erb :getboard
   end
+
+  post '/placeship' do
+    
+    @coord = params[:coord]
+    redirect:placeship
+
+  end
+
+  get '/getboard' do
+    game.player_1.place_ship Ship.battleship, :A5
+
+  end
+
+
+
 
   # get '/shoot' do
   #   $coord = params[:cell]
   #   erb :shoot
   # end
+
+
 
   run! if app_file == $0
 end
